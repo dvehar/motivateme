4aa6e469ce7d465919aa2f711d0a726c71e71761
dvehar <desmondvehar@gmail.com>
2016-12-24 19:29:32 -0800

    Use promises and fetch image and quote in parallel

--- lambda/motivateMe.js
+++ lambda/motivateMe.js
@@ -4,6 +4,7 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
+var RSVP = require('rsvp');
 
 var APP_NAME = 'Motivate Me Test';
 var PROD_APP_ID = 'amzn1.ask.skill.bd846ccf-84a7-4340-9249-5da185dfc1f7';
@@ -29,17 +30,13 @@ exports.handler = function (event, context) {
         }
 
         if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
+            onLaunch(event.request, event.session).then(function (responseValues) {
+                context.succeed(buildResponse(responseValues));
+            });
         } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
+            onIntent(event.request, event.session).then(function (responseValues) {
+                context.succeed(buildResponse(responseValues));
+            });
         } else if (event.request.type === "SessionEndedRequest") {
             onSessionEnded(event.request, event.session);
             context.succeed();
@@ -66,37 +63,53 @@ function onLaunch(launchRequest, session, callback) {
     console.log("onLaunch requestId=" + launchRequest.requestId
         + ", sessionId=" + session.sessionId);
 
-    var cardTitle = APP_NAME + "!";
-    getRandomMotivationalQuote(function (result) {
-        var response = new SSML();
-        response
-            .openSpeakTag()
-            .openParagraphTag()
-            .addPlainText(result.quote)
-            .closeParagraphTag()
-            .addPlainText(result.author)
-            .closeSpeakTag();
+    var promises = {
+        quoteAndCardText: getRandomMotivationalQuote().then(function (result) {
+            var response = new SSML();
+            response
+                .openSpeakTag()
+                .openParagraphTag()
+                .addPlainText(result.quote)
+                .closeParagraphTag()
+                .addPlainText(result.author)
+                .closeSpeakTag();
+            return {
+                quote: response.toString(),
+                cardText: getCardText(result)
+            };
+        }),
+        photoUrl: getRandomLandscapePhotoUrl()
+    };
 
-        getRandomLandscapePhotoUrl(function (photoUrl) {
-            callback(session.attributes, buildSpeechletResponseWithImage(cardTitle, response.toString(), getCardText(result), photoUrl, "", "true"));
-        });
+    return RSVP.hash(promises).then(function (results) {
+        var cardTitle = APP_NAME + "!";
+        return {
+            sessionAttributes: session.attributes,
+            speechletResponse: buildSpeechletResponseWithImage(cardTitle, results.quoteAndCardText.quote, results.quoteAndCardText.cardText, results.photoUrl, "", "true")
+        };
     });
 }
 
 /**
  * Called when the user specifies an intent for this skill.
  */
-function onIntent(intentRequest, session, callback) {
+function onIntent(intentRequest, session) {
     console.log("onIntent requestId=" + intentRequest.requestId
         + ", sessionId=" + session.sessionId
         + ", intentName=" + intentRequest.intent.name);
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
+    var intent = intentRequest.intent;
+    var intentName = intentRequest.intent.name;
 
     // dispatch custom intents to handlers here
     if (intentName == 'GetRandomMotivationQuote') {
-        handleIntentGetRandomMotivationQuote(intent, session, callback);
+        return handleIntentGetRandomMotivationQuote(intent, session).then(function (results) {
+            var cardTitle = APP_NAME + "!";
+            return {
+                sessionAttributes: session.attributes,
+                speechletResponse: buildSpeechletResponseWithImage(cardTitle, results.quoteAndCardText.quote, results.quoteAndCardText.cardText, results.photoUrl, "", "true")
+            };
+        });
     } else {
         throw "Invalid intent";
     }
@@ -114,7 +127,8 @@ function onSessionEnded(sessionEndedRequest, session) {
 }
 
 function handleIntentGetRandomMotivationQuote(intent, session, callback) {
-    getRandomMotivationalQuote(function (result) {
+    var promises = {
+      quoteAndCardText: getRandomMotivationalQuote().then(function (result) {
         var response = new SSML();
         response
             .openSpeakTag()
@@ -123,126 +137,147 @@ function handleIntentGetRandomMotivationQuote(intent, session, callback) {
             .closeParagraphTag()
             .addPlainText(result.author)
             .closeSpeakTag();
-        var cardTitle = APP_NAME + "!";
-        getRandomLandscapePhotoUrl(function (photoUrl) {
-            callback(session.attributes, buildSpeechletResponseWithImage(cardTitle, response.toString(), getCardText(result), photoUrl, "", "true"));
-        });
-    });
+        return {
+            quote: response.toString(),
+            cardText: getCardText(result)
+        }
+      }),
+      photoUrl: getRandomLandscapePhotoUrl()
+    };
+
+    return RSVP.hash(promises);
 }
 
 // ------- Helper functions to fetch quotes -------
 
-function getRandomMotivationalQuote (callback) {
+function getRandomMotivationalQuote () {
     var sources = [
         getRandomMotivationalQuoteSoure1,
         getRandomMotivationalQuoteSoure2/*,
         getRandomMotivationalQuoteSoure3*/
     ];
     var sourceToUse = sources[Math.floor(Math.random() * sources.length)];
-    sourceToUse(callback);
+
+    return sourceToUse();
 }
 
 // Go to a random page (0-9 inclusive) and pick a random quote
-function getRandomMotivationalQuoteSoure1 (callback) {
+function getRandomMotivationalQuoteSoure1 () {
     console.log('getRandomMotivationalQuoteSoure1');
     var min = 0;
     var max = 9;
     var pageNum = Math.floor(Math.random() * (max - min + 1)) + min;
     var page = (pageNum == min ? '': pageNum);
-    request('https://www.brainyquote.com/quotes/topics/topic_motivational' + page + '.html', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(body);
-        var quotes = $('div#quotesList div.bqQt');
-        if (quotes.length > 0) {
-            var randomIdx = Math.floor(Math.random() * quotes.length);
-            var rawQuote = quotes[randomIdx];
-            var quote = $('a[title="view quote"]', rawQuote).text();
-            var author = $('a[title="view author"]', rawQuote).text();
-            callback({
-                quote: quote,
-                author: author
-            });
-        } else {
-            throw('getRandomMotivationalQuoteSoure1 call failed: no quotes');
-        }
-      } else {
-        throw('getRandomMotivationalQuoteSoure1 call failed: ' + error);
-      }
+    var quote = new RSVP.Promise(function(resolve, reject) {
+        request('https://www.brainyquote.com/quotes/topics/topic_motivational' + page + '.html', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                var quotes = $('div#quotesList div.bqQt');
+                if (quotes.length > 0) {
+                    var randomIdx = Math.floor(Math.random() * quotes.length);
+                    var rawQuote = quotes[randomIdx];
+                    var quote = $('a[title="view quote"]', rawQuote).text();
+                    var author = $('a[title="view author"]', rawQuote).text();
+                    resolve({
+                        quote: quote,
+                        author: author
+                    });
+                } else {
+                    reject('getRandomMotivationalQuoteSoure1 call failed: no quotes');
+                }
+            } else {
+                reject('getRandomMotivationalQuoteSoure1 call failed: ' + error);
+            }
+        });
     });
+
+    return quote;   
 }
 
 // Go to a random page (1-9 inclusive) and pick a random quote
-function getRandomMotivationalQuoteSoure2 (callback) {
+function getRandomMotivationalQuoteSoure2 () {
     console.log('getRandomMotivationalQuoteSoure2');
     var min = 1;
     var max = 9;
     var pageNum = Math.floor(Math.random() * (max - min + 1)) + min;
     var page = (pageNum == min ? '': '/page/' + pageNum);
-    request('http://quotelicious.com/quotes/motivational-quotes' + page, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var $ = cheerio.load(body);
-        var quotes = $('div#content-quotespage div.post');
-        if (quotes.length > 0) {
-            var randomIdx = Math.floor(Math.random() * quotes.length);
-            var rawQuote = quotes[randomIdx];
-            var quote = $('a', rawQuote).text();
-            var author = $('em', rawQuote).text().substring(2).trim();
-            callback({
-                quote: quote,
-                author: author
-            });
-        } else {
-            throw('getRandomMotivationalQuoteSoure2 call failed: no quotes');
-        }
-      } else {
-        throw('getRandomMotivationalQuoteSoure2 call failed: ' + error);
-      }
+    var quote = new RSVP.Promise(function(resolve, reject) {
+        request('http://quotelicious.com/quotes/motivational-quotes' + page, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            var quotes = $('div#content-quotespage div.post');
+            if (quotes.length > 0) {
+                var randomIdx = Math.floor(Math.random() * quotes.length);
+                var rawQuote = quotes[randomIdx];
+                var quote = $('a', rawQuote).text();
+                var author = $('em', rawQuote).text().substring(2).trim();
+                resolve({
+                    quote: quote,
+                    author: author
+                });
+            } else {
+                reject('getRandomMotivationalQuoteSoure2 call failed: no quotes');
+            }
+          } else {
+            reject('getRandomMotivationalQuoteSoure2 call failed: ' + error);
+          }
+        });
     });
+
+    return quote;
 }
 
 // Hit an API for a random quote
 function getRandomMotivationalQuoteSoure3 (callback) {
     console.log('getRandomMotivationalQuoteSoure3');
-    request.post('http://www.quotationspage.com/random.php3', {"form": {"number":4, "collection[]": "motivate"}}, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body);
-            var quotes = $('dt.quote a[href *= "/quote/"]');
-            var authors = $('dd.author a[href *= "/quotes/"]');
-            if (quotes.length > 0) {
-                var randomIdx = Math.floor(Math.random() * quotes.length);
-                var quote = quotes[randomIdx].children[0].data.trim();
-                var author = authors[randomIdx].children[0].data.trim();
-                callback({
+    var quote = new RSVP.Promise(function(resolve, reject) {
+        request.post('http://www.quotationspage.com/random.php3', {"form": {"number":4, "collection[]": "motivate"}}, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                var quotes = $('dt.quote a[href *= "/quote/"]');
+                var authors = $('dd.author a[href *= "/quotes/"]');
+                if (quotes.length > 0) {
+                    var randomIdx = Math.floor(Math.random() * quotes.length);
+                    var quote = quotes[randomIdx].children[0].data.trim();
+                    var author = authors[randomIdx].children[0].data.trim();
+                    resolve({
+                        quote: quote,
+                        author: author
+                    });
+                } else {
+                    reject('getRandomMotivationalQuote3 call failed: no quotes');
+                }
+            } else {
+                reject('getRandomMotivationalQuote3 call failed: ' + error);
+            }
+        });
+    });
+
+    return quote;
+}
+
+function getRandomDesignQuote () {
+    console.log('getRandomDesignQuote');
+    var quote = new RSVP.Promise(function(resolve, reject) {
+        request('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var cleanBody = JSON.parse(body)[0];
+                var author = cleanBody.title;
+                var rawQuote = cleanBody.content; // '<p>.....</p>\n'
+                var cleanQuoteHtml = rawQuote.substring(0, rawQuote.length-1); // remove the newline
+                var $ = cheerio.load(cleanQuoteHtml);
+                var quote = $('p').text().trim();
+                resolve({
                     quote: quote,
                     author: author
                 });
-            } else {
-                throw('getRandomMotivationalQuote3 call failed: no quotes');
-            }
-        } else {
-            throw('getRandomMotivationalQuote3 call failed: ' + error);
-        }
+              } else {
+                reject('getRandomDesignQuote call failed: ' + error);
+              }
+        });
     });
-}
 
-function getRandomDesignQuote (callback) {
-    console.log('getRandomDesignQuote');
-    request('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var cleanBody = JSON.parse(body)[0];
-            var author = cleanBody.title;
-            var rawQuote = cleanBody.content; // '<p>.....</p>\n'
-            var cleanQuoteHtml = rawQuote.substring(0, rawQuote.length-1); // remove the newline
-            var $ = cheerio.load(cleanQuoteHtml);
-            var quote = $('p').text().trim();
-            callback({
-                quote: quote,
-                author: author
-            });
-          } else {
-            throw('getRandomDesignQuote call failed: ' + error);
-          }
-    });
+    return quote;
 }
 
 // ------- FLICKR Helper -------
@@ -251,15 +286,19 @@ function getFlickrUrl (farmId, serverId, photoId, photoSecret) {
     return 'https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + photoId + '_' + photoSecret + '.jpg';
 }
 
-function _getRandomLandscapePhotoUrlCount (callback) {
-    request('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' + process.env.FLICKR_KEY + '&text=landscape&license=7&sort=relevance&per_page=1', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body);
-            callback($('photos').attr('total'));
-        } else {
-            throw('getRandomLandscapePhotoUrl call failed: ' + error);
-        }
+function _getRandomLandscapePhotoUrlCount () {
+    var photoCount = new RSVP.Promise(function(resolve, reject) {
+        request('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' + process.env.FLICKR_KEY + '&text=landscape&license=7&sort=relevance&per_page=1', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                resolve($('photos').attr('total'));
+            } else {
+                reject('getRandomLandscapePhotoUrl call failed: ' + error);
+            }
+        });
     });
+
+    return photoCount;
 }
 
 function _getPageAndPhoto (photosPerPage, photoNumber) {
@@ -269,23 +308,27 @@ function _getPageAndPhoto (photosPerPage, photoNumber) {
     };
 }
 
-function getRandomLandscapePhotoUrl (callback) {
+function getRandomLandscapePhotoUrl () {
     var photosPerPage = 100;
     var randomPhoto = Math.floor(Math.random() * 300); // There are 500 results per page. Since we sort by relevence let's pick the top 300 to consider.
     var randomPageAndPhoto = _getPageAndPhoto(photosPerPage, randomPhoto);
-    request('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' + process.env.FLICKR_KEY + '&text=landscape&license=7&sort=relevance&per_page=' + photosPerPage + '&page=' + randomPageAndPhoto.page, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body);
-            var photo = $('photo')[randomPageAndPhoto.photoIdx];
-            var farmId = photo.attribs.farm;
-            var serverId = photo.attribs.server;
-            var photoId = photo.attribs.id;
-            var photoSecret = photo.attribs.secret;
-            callback(getFlickrUrl(farmId, serverId, photoId, photoSecret));
-        } else {
-            throw('getRandomLandscapePhotoUrl call failed: ' + error);
-        }
+    var quote = new RSVP.Promise(function(resolve, reject) {
+        request('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' + process.env.FLICKR_KEY + '&text=landscape&license=7&sort=relevance&per_page=' + photosPerPage + '&page=' + randomPageAndPhoto.page, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                var photo = $('photo')[randomPageAndPhoto.photoIdx];
+                var farmId = photo.attribs.farm;
+                var serverId = photo.attribs.server;
+                var photoId = photo.attribs.id;
+                var photoSecret = photo.attribs.secret;
+                resolve(getFlickrUrl(farmId, serverId, photoId, photoSecret));
+            } else {
+                reject('getRandomLandscapePhotoUrl call failed: ' + error);
+            }
+        });
     });
+
+    return quote;
 }
 
 // ------- SSML Helper -------
@@ -371,10 +414,10 @@ function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSessio
     };
 }
 
-function buildResponse(sessionAttributes, speechletResponse) {
+function buildResponse(responseValues) {
     return {
         version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
+        sessionAttributes: responseValues.sessionAttributes,
+        response: responseValues.speechletResponse
     };
 }
